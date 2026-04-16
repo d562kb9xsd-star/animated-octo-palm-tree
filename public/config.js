17a26cdb6716253f366ed71f56c76ee193ae8ed5
d562kb9xsd-star <gregwooden@hotmail.co.uk>
2026-04-16 14:13:32 +0100

    Update Supabase configuration values

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   siteName: 'UFO Archive Pro',
-  supabaseUrl: 'YOUR_SUPABASE_URL',
-  supabaseAnonKey: 'YOUR_SUPABASE_ANON_KEY'
+  supabaseUrl: 'https://fwqtcmzvtfthwwbveowi.supabase.co',
+  supabaseAnonKey: 'sb_publishable_LXOI7XQV3rLFO7RpokeQ-Q_z8tt2yJK'
 };
