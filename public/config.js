a3066ec996c07b48e94c7ffc775f2703e79e4e6d
d562kb9xsd-star <gregwooden@hotmail.co.uk>
2026-04-17 14:31:33 +0100

    Delete public/config.js

--- public/config.js
+++
@@ -1,5 +0,0 @@
-window.UFO_APP_CONFIG = {
-  siteName: 'UFO Archive Pro',
-  supabaseUrl: 'https://fwqtcmzvtfthwwbveowi.supabase.co',
-  supabaseAnonKey: 'sb_publishable_5ImXLmCaWeRzVKx_rTdgkQ_h4_xqG_b'
-};
