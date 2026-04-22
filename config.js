2777442d85d65744364fa9adc94799ac07f5323c
d562kb9xsd-star <gregwooden@hotmail.co.uk>
2026-04-22 08:52:12 +0100

    Update Supabase anon key placeholder in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.UFO_APP_CONFIG = {
   supabaseUrl: "https://fwqtcmzvftwhwweowoi.supabase.co",
-  supabaseAnonKey: "sb_publishable_5ImXLmCaWeRzVKx_rTdgkQ_h4_xqG_b"
+  supabaseAnonKey: "PASTE YOUR ufo_archive KEY HERE"
 };
