10e5c25d69033ac265cd6873248151ac7cf6d2bf
d562kb9xsd-star <gregwooden@hotmail.co.uk>
2026-04-22 09:49:48 +0100

    Update Supabase anon key in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.UFO_APP_CONFIG = {
   supabaseUrl: "https://https://pjzlxeeuumcxtumroppx.supabase.codo",
-  supabaseAnonKey: "PASTE_YOUR_PUBLISHABLE_KEY_HERE"
+  supabaseAnonKey: "pjzlxeeuumcxtumroppx"
 };
