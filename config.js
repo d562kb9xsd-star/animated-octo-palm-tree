31c704042df14ecbdafaa9f0757a64fc6dd3fe95
d562kb9xsd-star <gregwooden@hotmail.co.uk>
2026-04-22 09:42:09 +0100

    Update Supabase configuration values

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.UFO_APP_CONFIG = {
-  supabaseUrl: "https://fwqtcmzvftwhwweowoi.supabase.co",
-  supabaseAnonKey: "PASTE-YOUR-UFO_ARCHIVE-PUBLISHABLE-KEY-HERE"
+  supabaseUrl: "https://PASTE_YOUR_NEW_PROJECT_ID_HERE.supabase.co",
+  supabaseAnonKey: "PASTE_YOUR_PUBLISHABLE_KEY_HERE"
 };
